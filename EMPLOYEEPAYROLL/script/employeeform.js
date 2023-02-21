f55f3b29ebd1ddc9b095f7112b5a914047322df4
Amit0129 <amitkumarnayak40@gmail.com>
2023-02-22 02:04:06 +0530

    Data Sending to Jeson

--- EMPLOYEEPAYROLL/script/employeeform.js
+++ EMPLOYEEPAYROLL/script/employeeform.js
@@ -1,24 +1,38 @@
-var arr = [1,2,3,4,5,6];
-for(var i=0;i<arr.length;i++){
-    console.log(arr[i])
-}
-var num = 2;
-var count = 1;
-for(var i=0;i<arr.length;i++){
-    if(num==arr[i]){
-        console.log("Number"+" "+num+" "+"Found in Index"+" "+i)
-        break;
-    }
-    if(count==arr.length){
-        console.log("Number is not found")
-    }
-    count++;
-}
-if(arr.includes(num)){
-    console.log("number found")
-}
-else{
-    console.log("not found")
-}
+function submitForm() {
+    var name = $('#name').val();
+    var profile = $('input[name=profile]:checked').val();
+    var gender = $('input[name=gender]:checked').val();
+    var department = $('input[name=department]:checked').val();
+    var salary = $('#salary').val();
+    var day = $('#day').val();
+    var month = $('#month').val();
+    var year = $('#year').val();
+    var note = $('#note').val;
 
+    let reqPayload = {
+        "name": name,
+        "profileUrl": profile,
+        "gender": gender,
+        "departMent": department,
+        "salary": salary,
+        "day": day,
+        "month": month,
+        "year": year,
+        "notes": note
+    }
+    console.log(reqPayload);
 
+    $.ajax({
+        url: 'http://localhost:3000/employee',
+        type: 'POST',
+        dataType: 'json',
+        data: reqPayload,
+        success: function (data, textStatus, xhr) {
+            console.log(data);
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            console.log('Error in Operation');
+        }
+    });
+
+}
